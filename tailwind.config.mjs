18ba53c1dd54e6181129ae023bf49a06d6494365
Hachi-R <drive8hachi@gmail.com>
2025-01-12 16:12:00 -0300

    feat: add Satoshi font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,9 @@ export default {
 			screens: {
 				'xs': '425px',
 			},
+			fontFamily: {
+				sans: ['Satoshi', 'sans-serif'],
+			},
 			borderRadius: {
 				lg: 'var(--radius)',
 				md: 'calc(var(--radius) - 2px)',
